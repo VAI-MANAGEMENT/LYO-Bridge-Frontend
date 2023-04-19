187e4c2d259768132c63bb981bf825c9e3daebff
chathu-vai <chathurika.wijesinghe@vaimanagement.co>
2023-04-19 12:29:21 +0400

    prod env added

--- utils/urls.jsx
+++ utils/urls.jsx
@@ -1,6 +1,9 @@
 import constants from "./constants";
 
 const api_Link = () => {
+    if (constants.API_MODE == "prod") {
+        return process.env.X_API_PROD
+    }
     if (constants.API_MODE == "staging") {
         return process.env.X_API_STAGING
     }
